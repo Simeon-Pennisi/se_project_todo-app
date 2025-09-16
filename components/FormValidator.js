b36c350a453790b2c44825a19a87e565ef92f7f8
Simeon Pennisi <simeonpennisi@gmail.com>
2025-09-16 06:11:19 -0400

    feat: added FormValidator class components/FormValidator.js and exported to pages/index.js

--- components/FormValidator.js
+++ components/FormValidator.js
@@ -1,54 +1,23 @@
 class FormValidator {
-  constructor(validationConfig, inputElement) {
-    this._validationConfig = validationConfig;
-    this._inputElement = inputElement;
-
-    // private method to check field validity
-    this._isValid = (inputElement, errorElement) => {
-      if (!inputElement.validity.valid) {
-        errorElement.textContent = inputElement.validationMessage;
-        inputElement.classList.add(this._validationConfig.inputErrorClass);
-        errorElement.classList.add(this._validationConfig.errorClass);
-        console.log("invalid input");
-      } else {
-        errorElement.textContent = "";
-        inputElement.classList.remove(this._validationConfig.inputErrorClass);
-        errorElement.classList.remove(this._validationConfig.errorClass);
-        console.log("valid input");
-      }
-    };
-    // private method to change state of submit button
-    this._toggleButtonState = (inputList, buttonElement) => {
-      if (inputList.some((inputElement) => !inputElement.validity.valid)) {
-        buttonElement.classList.add(this._validationConfig.inactiveButtonClass);
-        buttonElement.disabled = true;
-        console.log("button disabled");
-      }
-    };
-    // public method to enable validation
-    this.enableValidation = () => {
-      const inputList = Array.from(
-        this._inputElement.querySelectorAll(
-          this._validationConfig.inputSelector
-        )
-      );
-      const buttonElement = this._inputElement.querySelector(
-        this._validationConfig.submitButtonSelector
-      );
-      // set initial state of button
-      this._toggleButtonState(inputList, buttonElement);
-      console.log("initial button state set");
-    };
-
-    //   create settings object to pass to the FormValidator class
-    // const validationConfig = {
-    //   formSelector: ".modal__form",
-    //   inputSelector: ".modal__input",
-    //   submitButtonSelector: ".modal__button",
-    //   errorClass: "modal__error_visible",
-    //   inputErrorClass: "modal__input_type_error",
-    //   inactiveButtonClass: "button_disabled",
-    // };
+  constructor(settings, formElement) {
+    this._settings = settings;
+    this._formElement = formElement;
+    console.log(this._settings);
+    this.formSelector = settings.formSelector;
+    (this._inputSelector = settings._inputSelector),
+      (this._submitButtonSelector = settings._submitButtonSelector),
+      (this._errorClass = settings._errorClass),
+      (this._inputErrorClass = settings._inputErrorClass),
+      (this._inactiveButtonClass = settings._inactiveButtonClass),
+      console.log(this._formElement);
+  }
+  enableValidation() {
+    const { inputSelector, submitButtonSelector } = this._settings;
+    this._inputList = Array.from(
+      this._formElement.querySelectorAll(inputSelector)
+    );
+    this._buttonElement = this._formElement.querySelector(submitButtonSelector);
+    this._setEventListeners();
   }
 }
 
